72df0991c8cb18dd40c649bb3e4389eeab4302dd
Dylan Freedman <freedmand@gmail.com>
2022-11-19 23:49:49 -0800

    feat: format syntax tree

--- src/parse/parse.ts
+++ src/parse/parse.ts
@@ -1,6 +1,7 @@
 import { parser } from "../generated/poly";
 import { Context, parseProgram } from "../generated/ast";
 import { printTree, syntaxHighlightProgram } from "../helpers/tree";
+import chalk from "chalk";
 
 const program = `fn X(a: Name) {
   let x = 2;
@@ -11,8 +12,47 @@ let x: Int | String = 2;
 
 const tree = parser.parse(program);
 
+const INDENT = chalk.gray("| ");
+
+function valueToString(value: any, indent: number): string {
+  if (typeof value === "string") {
+    return chalk.green(JSON.stringify(value));
+  } else if (typeof value === "number") {
+    return chalk.yellow(`${value}`);
+  } else if (value == null) {
+    return chalk.gray("null");
+  } else if (Array.isArray(value)) {
+    return `[
+${value
+  .map(
+    (subvalue) =>
+      `${INDENT.repeat(indent + 1)}${valueToString(subvalue, indent + 1)}`
+  )
+  .join("\n")}
+${INDENT.repeat(indent)}]`;
+  } else {
+    return astToString(value, indent);
+  }
+}
+
+function astToString(ast: { kind: string }, indent = 0): string {
+  const keys = Object.keys(ast).filter((x) => x !== "kind");
+  return `${chalk.cyan(ast.kind)} {
+${keys
+  .map(
+    (key) =>
+      `${INDENT.repeat(indent + 1)}${chalk.blue(key)}: ${valueToString(
+        ast[key],
+        indent + 1
+      )}`
+  )
+  .join("\n")}
+${INDENT.repeat(indent)}}`;
+}
+
 // printTree(program, parser.parse(program));
-console.log(parseProgram(new Context(program), tree.topNode));
+// console.log(parseProgram(new Context(program), tree.topNode));
+console.log(astToString(parseProgram(new Context(program), tree.topNode)));
 // console.log(syntaxHighlightProgram(program, tree));
 
 // console.log(parser.parse(program).children[0]);
